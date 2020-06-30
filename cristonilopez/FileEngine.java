6e2fe5fa16db72e1e7d51df604a5038f779ed77d
Davide Cristoni <davide@cristoni.it>
2020-06-30 18:17:06 +0200

    aggiunto supporto primordiale lettura da file

---
+++ cristonilopez/FileEngine.java
@@ -0,0 +1,133 @@
+package movida.cristonilopez;
+
+import movida.commons.*;
+import java.util.Scanner;
+
+public class FileEngine {
+    
+    /** Lettura del numero di voti del film
+     * 
+     * Restituisce il numero di voti ricevuti
+     * dal film
+     * 
+     * @param file Scanner che legge dal file selezionato
+	 * @return numero di voti del film
+	 */
+    public static Integer getVotes(Scanner file) {
+        try
+        {
+        String temp = file.nextLine();
+        int votes = 0;
+        temp = temp.substring(temp.indexOf(':')+1);
+        for(int i = 0; i<temp.length(); i++)
+        {
+            if(Character.isDigit(temp.charAt(i)))
+            {
+                votes = votes*10 + Character.getNumericValue(temp.charAt(i));
+            }
+        }
+        return votes;
+        }
+        catch (Exception e)
+        {
+            throw new MovidaFileException();
+        }
+    }
+
+    /**Ottenimento lista degli attori
+     * 
+     * Legge dallo Scanner gli attori partecipanti
+     * a un film e ne restituisce l'array
+     * 
+     * @param file Scanner impostato sulla lettura da file
+     * @return array di Person contenente tutti gli attori del film
+     */
+    public static Person[] getCast(Scanner file) {
+        String cast = file.nextLine();
+        cast = cast.substring(cast.indexOf(' ')); //non gestisco il +1 perché lo gestisco direttamente nel ciclo
+        int i=0, j=0;
+        while(cast.indexOf(',', j+1) > 0) //non prevede che in un film ci sia solo un attore
+        {
+            j = cast.indexOf(',', j+1);
+            i++;
+        }
+        Person[] persone = new Person[i];
+        j = 0;
+        i = 0;
+        while(cast.indexOf(',', j+1) > 0)
+        {
+            persone[i++] = new Person(cast.substring(j+1, cast.indexOf(',', j+1)));
+            j = cast.indexOf(',', j+1);
+        }
+        return persone;
+    }
+
+    /**Ottiene il direttore
+     * 
+     * Legge dallo scanner il direttore del film e lo restituisce
+     * 
+     * @param fileScanner impostato sula lettura da file
+     * @return Person contenente il direttore del film
+     */
+    public static Person getDirector(Scanner file) { 
+        try{
+        String directort = file.nextLine();
+        directort = directort.substring(directort.indexOf(' ')+1);
+        Person director = new Person(directort);
+        return director;
+        }
+        catch (Exception e)
+        {
+            throw new MovidaFileException();
+        }
+    }
+
+    /**Ottiene l'anno di produzione
+     * 
+     * Legge dallo Scanner impostato su file l'anno di direzione del film
+     * 
+     * @param file Scanner impostato sulla lettura da file
+     * @return anno di produzione del film
+     */
+    public static Integer getYear(Scanner file) {
+        try
+        {
+        String temp = file.nextLine();
+        temp = temp.substring(temp.indexOf(':')+1);
+        Integer year = 0; //prende la riga contenente l'anno di pubblicaazione, ritaglia via la stringa iniziale e converte in int TODO aggiungere controllo
+        for(int i = 0; i<temp.length(); i++)
+        {
+            if(Character.isDigit(temp.charAt(i)))
+            {
+                year = year*10 + Character.getNumericValue(temp.charAt(i));
+            }
+        }
+        return year;
+        }
+        catch (Exception e)
+        {
+            throw new MovidaFileException();
+        }
+    }
+
+    /**Otiene il nome del film
+     * 
+     * Legge dallo Scanner impostato su file il nome del film
+     * 
+     * @param file Scanner impostato su file
+     * @return il nome del film
+     */
+    public static String getTitle(Scanner file) {
+        try
+        {
+        String title = file.nextLine();
+        title = title.substring(title.indexOf(' ')+1);
+        return title; //TODO tagliare la strnga dopo il nome del film per rimuovere qualsiasi cosa ci sia dopo che non ci interessa
+        }
+        catch (Exception e)
+        {
+            throw new MovidaFileException();
+        }
+    }
+
+}
--- cristonilopez/MovidaCore.java
+++ cristonilopez/MovidaCore.java
@@ -1,5 +1,137 @@
 package movida.cristonilopez;
 
-public class MovidaCore{
+import movida.commons.*;
+import movida.cristonilopez.maps.Dizionario;
+import movida.cristonilopez.maps.albero23.Albero23;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MovidaCore implements IMovidaDB, IMovidaConfig {
+
+    SortingAlgorithm sort;
+    MapImplementation map;
+
+    Dizionario struttura;
+
+    @Override
+    public boolean setSort(SortingAlgorithm a) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean setMap(MapImplementation m) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void loadFromFile(File f) {
+        if(!f.exists())
+        {
+            throw new MovidaFileException();
+        }
+
+        Scanner file = null;
+        try
+        {
+            file = new Scanner(f);
+        } 
+        catch (FileNotFoundException e)
+        {
+            throw new MovidaFileException();
+        }
+        struttura = new Albero23(); //aggiungere controllo se esiste già na struttura e aggiungere selezione di struttura
+        boolean continua = true;
+        while(continua)
+        {
+            String title = FileEngine.getTitle(file); //potrebbe essere più utile accorpare tutti questi metodi in un solo
+            
+            Integer year = FileEngine.getYear(file);  //metodo dato che questi metodi non possono assolutamente essere 
+            
+            Person director = FileEngine.getDirector(file);//autilizzati in un ordine diverso
+            
+            Person[] cast = FileEngine.getCast(file);
+
+            Integer votes = FileEngine.getVotes(file);
+            
+            Movie result = new Movie(title, year, votes, cast, director);
+
+            Test.stampa(result);
+            struttura.insert(result, title);
+            //far smaltire la riga vuota, TODO da sistemare
+            if(!file.hasNextLine())
+            {
+                continua = false;
+            }
+            else
+            {
+                String temp = file.nextLine();
+                if(temp.length() >= 4 || !file.hasNextLine()) //TODO qua prevede che sia formattata male, ma anzichè fermarmi io devo dare err
+                {
+                    continua = false;
+                }
+            }
+        } 
+        file.close();
+    }
+
+
+
+    @Override
+    public void saveToFile(File f) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int countMovies() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int countPeople() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean deleteMovieByTitle(String title) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Movie getMovieByTitle(String title) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Person getPersonByName(String name) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Movie[] getAllMovies() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Person[] getAllPeople() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
